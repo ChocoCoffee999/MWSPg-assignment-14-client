0e698e52b3802a5d0eee0228f02c4bddf1d7acf0
ChocoCoffee999 <kim99602@naver.com>
2024-06-12 21:54:03 +0900

    android client for assignment 14 in Mobile Web Service Programming

--- app/src/main/java/com/example/assignment14_2/MainActivity.java
+++ app/src/main/java/com/example/assignment14_2/MainActivity.java
@@ -24,13 +24,23 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private static final int READ_MEDIA_IMAGES_PERMISSION_CODE = 1001;  // 상수 정의
@@ -44,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
 
     private final ExecutorService executorService = Executors.newSingleThreadExecutor();
     private final Handler handler = new Handler(Looper.getMainLooper());
+
+    String twoHyphens = "--";
+    String boundary = "*****" + Long.toString(System.currentTimeMillis()) + "*****";
+    String lineEnd = "\r\n";
 
     private final ActivityResultLauncher<Intent> imagePickerLauncher = registerForActivityResult( //...코드 계속
             new ActivityResultContracts.StartActivityForResult(),
@@ -132,33 +146,85 @@ public class MainActivity extends AppCompatActivity {
 
     private String uploadImage(String imageUrl) throws IOException, JSONException {
         OutputStreamWriter outputStreamWriter = null;
+        DataOutputStream outputStream = null;
+
         try {
             try {
-                URL url = new URL(UPLOAD_URL);
+                URL url = new URL(UPLOAD_URL_PYTHON_ANYWHERE);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("POST");
-                connection.setRequestProperty("Authorization", "JWT c0c06532928e0d2374d8e462d3a62cc750edf157");
-                //connection.setRequestProperty("Authorization", "JWT d0ed741045ce342106954398c943fc9d28457246"); // pythonanywhere token
-                connection.setRequestProperty("Content-Type", "application/json");
 
-                JSONObject jsonObject = new JSONObject();
+                //Authorization, Token
+                //connection.setRequestProperty("Authorization", "JWT c0c06532928e0d2374d8e462d3a62cc750edf157");
+                connection.setRequestProperty("Authorization", "JWT d0ed741045ce342106954398c943fc9d28457246"); // pythonanywhere token
+
+                //Content-Type
+                //connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Connection", "Keep-Alive");
+                connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+                //JSONObject jsonObject = new JSONObject();
                 //jsonObject.put("author", "1"); token을 이용해 작성자를 확인
-                jsonObject.put("title", "안드로이드-REST API 테스트");
-                jsonObject.put("text", "안드로이드로 작성된 REST API 테스트 입력 입니다.");
-                jsonObject.put("created_date", "2024-06-03T18:34:00+09:00");
-                jsonObject.put("published_date", "2024-06-03T18:34:00+09:00");
+                //jsonObject.put("title", "안드로이드-REST API 테스트");
+                //jsonObject.put("text", "안드로이드로 작성된 REST API 테스트 입력 입니다.");
+                //jsonObject.put("created_date", "2024-06-03T18:34:00+09:00");
+                //jsonObject.put("published_date", "2024-06-03T18:34:00+09:00");
                 //jsonObject.put("image", imageUrl);
 
-                outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
-                outputStreamWriter.write(jsonObject.toString());
-                outputStreamWriter.flush();
+                //outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
+                //outputStreamWriter.write(jsonObject.toString());
+                //outputStreamWriter.flush();
 
-                connection.connect();
+//                if (connection.getResponseCode() == 200 || connection.getResponseCode() == 201) {
+//                    Log.e("uploadImage", "Success");
+//                }
+//                connection.disconnect();
 
-                if (connection.getResponseCode() == 200 || connection.getResponseCode() == 201) {
-                    Log.e("uploadImage", "Success");
+                outputStream = new DataOutputStream(connection.getOutputStream());
+
+                File imageFile = new File(imageUrl);
+
+                if (!imageFile.isFile()) {
+                    System.out.println("Image file not found: " + imageUrl);
                 }
-                connection.disconnect();
+                Log.i("Test", "A");
+                FileInputStream fileInputStream = new FileInputStream(imageFile);
+                byte[] fileBytes = new byte[(int) imageFile.length()];
+                fileInputStream.read(fileBytes);
+                fileInputStream.close();
+                Log.i("Test", "B");
+
+                outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                outputStream.writeBytes("Content-Disposition: form-data; name=\"image\";filename=\"" + imageFile.getName() + "\"\r\n");
+                outputStream.writeBytes(lineEnd);
+                outputStream.write(fileBytes);
+                outputStream.writeBytes(lineEnd);
+
+                Log.i("Test", "C");
+
+                addFormField(outputStream, "title", "안드로이드-REST API 테스트");
+                addFormField(outputStream, "text", "안드로이드로 작성된 REST API 테스트 입력 입니다.");
+                addFormField(outputStream, "created_date", "2024-06-03T18:34:00+09:00");
+                addFormField(outputStream, "published_date", "2024-06-03T18:34:00+09:00");
+
+                Log.i("Test", "D");
+
+                outputStream.writeBytes("--" + boundary + "--\r\n");
+                outputStream.flush();
+                outputStream.close();
+
+                Log.i("Test", "E");
+
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+                System.out.println("Response: " + response.toString());
+
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
             } catch (IOException e) {
@@ -168,5 +234,13 @@ public class MainActivity extends AppCompatActivity {
             Log.e("uploadImage", "Exception in uploadImage: " + e.getMessage());
         }
         return "success";
+    }
+
+    private void addFormField(DataOutputStream outputStream, String fieldName, String value) throws IOException {
+        outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        outputStream.writeBytes("Content-Disposition: form-data; name=\"" + fieldName + "\"" + lineEnd);
+        outputStream.writeBytes(lineEnd);
+        outputStream.write(value.getBytes(StandardCharsets.UTF_8));
+        outputStream.writeBytes(lineEnd);
     }
 }
